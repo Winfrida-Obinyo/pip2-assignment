ce9d7b2f893df0e55a5363aa86b45f4fc74ac926
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-05-05 20:13:45 +0300

    added two tasks

--- pip2.js
+++ pip2.js
@@ -29,7 +29,7 @@ console.log(arraySum);
 
 // Write a function that takes two numbers as 
 // input and returns true if their sum is greater than 100, and false otherwise.
-function isSumGreaterThan100(num1, num2) {
+function sumGreaterThan100(num1, num2) {
     const sum = num1 + num2;
     if (sum > 100) {
       return true;
@@ -37,10 +37,44 @@ function isSumGreaterThan100(num1, num2) {
       return false;
     }
   }
-const result2 = isSumGreaterThan100(10, 90);
+const result2 = sumGreaterThan100(10, 90);
 console.log(result2); 
-  
-  
+
+// Write a function that takes in a array of numbers as a parameter and returns the second largest number in the array
+
+function secondLargest(array){
+let sorting=array.sort((a,b)=>{
+  return a-b
+})  
+let second=sorting.length-2
+let final=sorting[second]
+console.log(final);
+
+
+}
+let array=[3,53,63,13,86,4]
+secondLargest(array)
+
+
+// Write a function that takes a string as a parameter and returns true if the string is a palindrome and false otherwise
+
+function palindrome(same) {
+  let arry= same.split('').reverse().join("")
+  if (arry===same) {
+    return true
+  }
+  else{
+    return false
+  }
+}
+ let same = "banana"
+ console.log(palindrome(same))
+
+
+
+
+
+
   
 
 
